3bc352465d9214ece1dc5151fb7b86b15c554f3d
Princess <ppalmer04@yahoo.com>
2014-01-24 15:30:37 -0600

    more changes to helloGit

--- src/helloGit.java
+++ src/helloGit.java
@@ -6,6 +6,12 @@ public class helloGit
 	{
 		System.out.println("This is a Git Demo!");
 		System.out.println("Practice");
+		
+		for(int i=1; i <=10; i++)
+		{
+			int sum = i * 10;
+			System.out.printf("The sum is equal to " + sum +".\n");
+		}
 
 	}
 
